33a49efff11f8b9017a75cfc42d99eb3c4598f02
Todd Trimakas <todd.trimakas@gmail.com>
2018-09-26 13:22:46 +0000

    able to view logging in AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
 config.webpacker.check_yarn_integrity = false
 
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
