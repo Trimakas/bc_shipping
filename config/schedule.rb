94f2684d4263aded4b893a8c959f1f72f50bd984
dmkonst <warqueq@gmail.com>
2019-09-26 11:47:14 +0300

    Add .envs file in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,11 @@
+envs_path = File.expand_path('~/.envs')
+if File.exists?(envs_path)
+  set :job_template, "/bin/bash -l -c '. #{envs_path} && :job'"
+else
+  puts ("\e[0;33;49mWARNING: #{envs_path} is not found. You may have to create it and set your " +
+        "environment variables in it for this rake task to work properly.\e[0m")
+end
+
 every 10.minutes do
   rake "poll_sqs_queue"
 end
